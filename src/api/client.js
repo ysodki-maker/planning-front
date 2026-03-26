50c7e0a0bfcb60a5006bcbbb5a34add9b6cdcd4b
ysodki-maker <ysodki@virtualconnect.ma>
2026-03-26 18:17:26 +0100

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://planning.cosinus.ma/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
