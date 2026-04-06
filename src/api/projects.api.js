ca4b0c68ba794a56c0cfe690fc49a90221e329b9
ysodki-maker <ysodki@virtualconnect.ma>
2026-04-06 10:04:26 +0100

    Update projects.api.js

--- src/api/projects.api.js
+++ src/api/projects.api.js
@@ -8,6 +8,7 @@ export const projectsApi = {
   create:      (data)   => api.post('/projects', data),
   update:      (id, data) => api.put(`/projects/${id}`, data),
   delete:      (id)     => api.delete(`/projects/${id}`),
+  confirm:     (id, data) => api.post(`/projects/${id}/confirm`, data || {}),
   assignUsers: (id, data) => api.post(`/projects/${id}/assign`, data),
   removeUser:  (id, userId) => api.delete(`/projects/${id}/assign/${userId}`),
 };
